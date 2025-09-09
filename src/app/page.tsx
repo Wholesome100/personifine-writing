ba713d3760d757e9cd18409d54dc3404e42357f8
Wholesome100 <masteryuyda@gmail.com>
2025-09-08 20:20:54 -0400

    set up import map for deno

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+import Footer from "@/components/Footer.tsx";
+import Header from "@/components/Header.tsx";
 
 export default function Home() {
   return (
