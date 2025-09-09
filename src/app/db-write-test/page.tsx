d57a311ea2e85fbbe889cfa0a0a48ca561a34cce
Wholesome100 <masteryuyda@gmail.com>
2025-09-09 14:24:42 -0400

    add in test pages for neon

---
+++ src/app/db-write-test/page.tsx
@@ -0,0 +1,16 @@
+import { neon } from '@neondatabase/serverless';
+export default async function Page() {
+  async function create(formData: FormData) {
+    "use server";
+    const sql = neon(process.env.DATABASE_URL!);
+    await sql`CREATE TABLE IF NOT EXISTS comments (comment TEXT)`;
+    const comment = formData.get("comment");
+     await sql`INSERT INTO comments (comment) VALUES (${comment})`;
+  }
+  return (
+    <form action={create}>
+      <input type="text" placeholder="write a comment" name="comment" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
