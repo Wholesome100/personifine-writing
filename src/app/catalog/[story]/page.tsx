3c11a8209c936262b8ec853a4cf6245338be861b
Wholesome100 <masteryuyda@gmail.com>
2025-09-17 15:02:43 -0400

    Implement fetches for story and chapter metadata using url slug params

--- src/app/catalog/[story]/page.tsx
+++ src/app/catalog/[story]/page.tsx
@@ -1,3 +1,38 @@
-export default function Story() {
-  return <span>This is the page with an overview of the current story.</span>;
+import { sql } from "@/db/context";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+async function getStoryDetails(slug: string) {
+  const response =
+    await sql.query("SELECT story_id, title, summary FROM stories WHERE slug = $1", [slug]);
+  return response;
+}
+
+async function getChapterDetails(story_id: string) {
+  const response =
+    await sql.query("SELECT chapter_id, title, description FROM chapters WHERE story_ID = $1 ORDER BY sequence ASC",
+      [story_id]
+    );
+  return response;
+}
+
+export default async function Story(
+  { params }: { params: Promise<{ story: string }> },
+) {
+  const { story } = await params;
+  console.log(story)
+  const metadata = await getStoryDetails(story)
+  const chapters = await getChapterDetails(metadata[0]?.story_id)
+  console.log(metadata)
+  console.log(chapters)
+
+  return (
+    
+    <div>
+      <Header />
+      <span>This is the page with an overview of the current story.</span>;
+      <span>{story}</span>
+      <Footer />
+    </div>
+  );
 }
