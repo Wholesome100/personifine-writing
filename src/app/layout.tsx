c6f3bfaaa9b9ebb98ae499f4b76d1086d392024c
Wholesome100 <masteryuyda@gmail.com>
2025-09-29 13:53:39 -0400

    add metadata and robots.txt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,31 @@ const merriweather_sans = Merriweather_Sans({
   subsets: ["latin"],
 });
 
+// NOTE: Favicon needs to be a multiple of 48
+// Sitemap can be looked into in the future
 export const metadata: Metadata = {
-  title: "Personifine",
-  description: "Bringing stories to life~",
+  metadataBase: new URL("https://personifine.com"),
+  title: { template: "%s | Personifine", default: "Personifine" },
+  description: "Bringing stories to life!",
+  generator: "Next.js",
+  applicationName: "personifine-writing",
+  keywords: [
+    "writing",
+    "persona",
+    "personify",
+    "refine",
+    "personified",
+    "wioleta",
+  ],
+  creator: "Wholesome100",
+  openGraph: {
+    title: "Personifine",
+    description: "Bringing stories to life!",
+    url: "https://personifine.com",
+    siteName: "Personifine",
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
